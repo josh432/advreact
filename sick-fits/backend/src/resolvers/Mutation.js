d4d75c451ebeff2744c3a35571d268d89d42c7af
Josh Silverman <jrsilverman71@hotmail.com>
2018-11-12 12:15:07 -0800

    added item edit functionality

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,24 +1,35 @@
 const Mutations = {
-    async createItem(parent, args, ctx, info) {
-        //TODO: Check if they are logged in
-        const item = await ctx.db.mutation.createItem({
-            data: {
-               ...args
-            }
-        }, info);
+  async createItem(parent, args, ctx, info) {
+    //TODO: Check if they are logged in
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
 
-        console.log(item);
+    console.log(item);
 
-        return item;
-    }
-    // createDog(parent, args, ctx, info) {
-    //     global.dogs = global.dogs || [];
-    //     //create a dog
-    //     const newDog = { name: args.name };
-    //     global.dogs.push(newDog);
-    //     return newDog;
-    // },
-
+    return item;
+  },
+  updateItem(parent, args, ctx, info) {
+    //first take a copy of the updates
+    const updates = { ...args };
+    //remove the ID from the updates
+    delete updates.id;
+    //run the update method
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id
+        },
+      },
+      info
+    );
+  },
 };
 
 module.exports = Mutations;
